03f0abd5f6d3d84928947b22e572e6babdd46c37
charlesafuh <76530004+charlesafuh@users.noreply.github.com>
2024-10-25 16:39:46 -0400

    Update app.py
    v2

--- examples/first-docker-file/app.py
+++ examples/first-docker-file/app.py
@@ -1,1 +1,2 @@
-print("Hello World")
+print("Hello World from the great continet of Africa. This focus on Africa")
+print("We empowered and educate the continent of Africa. The pride of our Homeland")
